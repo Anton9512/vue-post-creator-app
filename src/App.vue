9ff9c72564238a8fdbdf9fc1c05d1e74441039ef
Anton9512 <anton.mail9512@gmail.com>
2022-02-08 11:00:43 +0300

    add PostList & PostForm

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="app">
-        <form>
-            <h4>Создание поста</h4>
-            <input v-model="title" class="input" type="text" placeholder="Название">
-            <input v-model="body" class="input" type="text" placeholder="Описание">
-            <button class="btn" @click.prevent="createPost">Создать</button>
-        </form>
-        <div class="post" v-for="post in posts" :key="posts.id">
-            <div><b>Название: </b>{{ post.title }}</div>
-            <div><b>Описание: </b>{{ post.body }}</div>
-        </div>
+      <post-form @create="createPost"/>
+      <post-list
+              :posts="posts"
+      />
     </div>
 </template>
 
 <script>
+  import PostList from "@/components/PostList";
+  import PostForm from "@/components/PostForm";
     export default {
+      components: {
+        PostForm,
+        PostList
+      },
         data() {
             return {
                 posts: [
@@ -22,20 +22,11 @@
                     {id: 2, title: 'js 2', body: 'description js 2'},
                     {id: 3, title: 'js 3', body: 'description js 3'},
                 ],
-                title: '',
-                body: '',
             };
         },
         methods: {
-            createPost() {
-                const newPost = {
-                    id: Date.now(),
-                    title: this.title,
-                    body: this.body,
-                }
-                this.posts.unshift(newPost)
-                this.title = '';
-                this.body = '';
+            createPost(post) {
+                this.posts.unshift(post)
             }
         }
     };
@@ -53,32 +44,4 @@
         padding: 20px;
     }
 
-    .input {
-        width: 100%;
-        border: 1px solid teal;
-        margin-top: 15px;
-        padding: 10px 15px;
-    }
-
-    form {
-        display: flex;
-        flex-direction: column;
-
-    }
-
-    .btn {
-        margin: 15px 0;
-        align-self: flex-end;
-        padding: 10px 15px;
-        background: transparent;
-        color: teal;
-        border: 1px solid teal;
-        cursor: pointer;
-    }
-
-    .post {
-        border: 2px solid teal;
-        margin-top: 15px;
-        padding: 15px;
-    }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post" v-for="post in posts" :key="posts.id">
+        <div><b>Название: </b>{{ post.title }}</div>
+        <div><b>Описание: </b>{{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            posts: {
+                type: Array,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .post {
+        border: 2px solid teal;
+        margin-top: 15px;
+        padding: 15px;
+    }
+</style>
