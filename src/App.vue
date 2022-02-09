c8653e1ab82683282e05613e129fe84ca166fc54
Anton9512 <anton.mail9512@gmail.com>
2022-02-09 12:39:57 +0300

    create dialog window

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="app">
-      <post-form @create="createPost"/>
+        <h1>Страница с постами</h1>
+        <my-button @click="showDialog">
+            Создать пост
+        </my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form @create="createPost"/>
+        </my-dialog>
       <post-list
               :posts="posts"
               @remove="removePost"
@@ -11,8 +17,12 @@
 <script>
   import PostList from "@/components/PostList";
   import PostForm from "@/components/PostForm";
+  import MyDialog from "./components/UI/MyDialog";
+  import MyButton from "./components/UI/MyButton";
     export default {
       components: {
+          MyButton,
+          MyDialog,
         PostForm,
         PostList
       },
@@ -23,15 +33,20 @@
                     {id: 2, title: 'js 2', body: 'description js 2'},
                     {id: 3, title: 'js 3', body: 'description js 3'},
                 ],
+                dialogVisible: false,
             };
         },
         methods: {
             createPost(post) {
                 this.posts.unshift(post)
+                this.dialogVisible = false;
             },
             removePost(post) {
                 this.posts = this.posts.filter(p => p.id !== post.id)
-            }
+            },
+            showDialog() {
+                this.dialogVisible = true
+            },
         }
     };
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-if="posts.length > 0">
         <h3>Список постов</h3>
         <post-item
                 @remove="$emit('remove', post)"
@@ -7,6 +7,9 @@
                 v-for="post in posts"
                 :key="posts.id"
         />
+    </div>
+    <div v-else style="color: tomato; font-size: 20px;">
+        Список постов пуст
     </div>
 </template>
 
